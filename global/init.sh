33045c567dab4b754db7553ff41849b08e625036
Denis Craig <dcraig@thousandeyes.com>
2018-12-03 19:36:42 +0000

    Run link as sudo

--- global/init.sh
+++ global/init.sh
@@ -5,4 +5,4 @@ sudo yum update
 sudo yum install -y python34-pip awslogs
 
 # Always use UTC for logs !
-ln -fs /usr/share/zoneinfo/UTC /etc/localtime
+sudo ln -fs /usr/share/zoneinfo/UTC /etc/localtime
