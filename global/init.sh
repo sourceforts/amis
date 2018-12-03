0e622f16af4e71d90ad30d64d07733c73e295ded
Denis Craig <dcraig@thousandeyes.com>
2018-12-03 19:54:06 +0000

    Use correct python version for installing pip

--- global/init.sh
+++ global/init.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Init script to install required dependencies on all images
 
+echo "Updating package manager..."
 sudo yum update
-sudo yum install -y python34-pip awslogs
+echo "Done."
+
+echo "Installing dependencies..."
+sudo yum install -y python27-pip awslogs
+echo "Done."
 
 # Always use UTC for logs !
+echo "Setting local time to UTC..."
 sudo ln -fs /usr/share/zoneinfo/UTC /etc/localtime
+echo "Done."
